a8cf59c3a110ce434c7b263d98f2a1170a5b6cb0
İskender Akyüz <akyuziskender@hotmail.com>
2019-04-04 20:47:38 +0300

    Create GroundChecker.cs

---
+++ no-pact/Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+	private Character character;
+
+    // Start is called before the first frame update
+    void Start() {
+		character = transform.parent.GetComponent<Character>();
+    }
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (null == character) {
+			Start();
+		}
+		if (other.CompareTag("Ground"))  {
+			character.grounded = true;
+			character.canUseSpeciality = false;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if (other.CompareTag("Ground")) {
+				character.grounded = true;
+			character.canUseSpeciality = false;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.CompareTag("Ground")) {
+			character.grounded = false;
+			character.canUseSpeciality = true;
+		}
+	}
+}
